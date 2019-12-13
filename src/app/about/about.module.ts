b6f23122abeae8f74b9e9b10de4bab0667952107
Vincent Tran <Vincent.Tran@jbhunt.com>
2019-12-13 15:21:11 -0600

    Added PrimeNG and Awesome-Font

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
 import { AboutRoutingModule } from './about-routing.module';
 
-import { HeaderModule } from '../core/header/header.module'; 
+import { HeaderModule } from '../core/header/header.module';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [ AboutComponent ],
   imports: [
     CommonModule,
     AboutRoutingModule,
-    HeaderModule 
+    HeaderModule,
+    ButtonModule
   ],
   exports: [ AboutComponent ]
 })
