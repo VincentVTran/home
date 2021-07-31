b472a46dc4f44fd6611d920b389f02ea6b40bcfd
Vincent Tran <vincenttran197120@gmail.com>
2021-07-31 15:50:04 -0500

    Added new pictures for portfolio

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -66,14 +66,7 @@ class Header extends Component {
             </Fade>
             <hr />
             <Fade bottom duration={2000}>
-              <ul className="social">
-                <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
-                </a>
-                <a href={github} className="button btn github-btn">
-                  <i className="fa fa-github"></i>Github
-                </a>
-              </ul>
+              <ul className="social"></ul>
             </Fade>
           </div>
         </div>
