8876a5624d0baad2f72684646a8e3b4939d9f3c1
Vincent Tran <vincenttran197120@gmail.com>
2021-08-01 22:00:32 -0500

    Removed contact tab

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -47,12 +47,7 @@ class Header extends Component {
                 Works
               </a>
             </li>
-
-            <li>
-              <a className="smoothscroll" href="#contact">
-                Contact
-              </a>
-            </li>
+            
           </ul>
         </nav>
 
